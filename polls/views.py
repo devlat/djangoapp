f2714495f3ac9fa1a2ac020d70cd13f44e0b66d0
Ilyas Latif <illatiph@gmail.com>
2017-10-15 21:38:56 +0500

    Init

---
+++ polls/views.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.http import HttpResponse
+
+from django.shortcuts import render
+from polls.models import Question, Choice
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ",\n".join([q.question_text for q in latest_question_list])
+    context = {'latest_question_list' : latest_question_list}
+
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
